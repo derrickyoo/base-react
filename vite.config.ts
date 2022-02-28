19613f92749661c1731f55387f1f34df043196e4
Derrick Yoo <4998684+derrickyoo@users.noreply.github.com>
2022-02-27 23:30:48 -0800

    Confg vite to use babel plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import macrosPlugin from "vite-plugin-babel-macros";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    macrosPlugin()
+    react({
+      babel: {
+        babelrc: true
+      }
+    })
   ]
 })
