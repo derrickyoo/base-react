94943d7f615de75871ddf8542681bfad29159262
Derrick Yoo <derrickyoo@gmail.com>
2022-03-07 15:22:41 -0800

    Add utils to be used in GlobalStyles

---
+++ src/utils/typography.ts
@@ -0,0 +1,20 @@
+export const WEIGHTS = {
+  normal: 400,
+  medium: 550,
+  bold: 700
+};
+
+export const TYPESCALE = {
+  header1: "4.209rem",
+  header2: "3.157rem",
+  header3: "2.369rem",
+  header4: "1.777rem",
+  header5: "1.333rem",
+  paragraph: "1rem",
+  small: "0.75rem"
+}
+
+export const FAMILIES = {
+  base: "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
+  mono: "source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace"
+}
