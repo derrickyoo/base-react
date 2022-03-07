4549d1bbaefff4d45fc6b59d8ead5851d146cf3f
Derrick Yoo <derrickyoo@gmail.com>
2022-03-07 13:11:44 -0800

    Remove index.css and add GlobalStyles

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import { StrictMode } from "react";
 import { render } from "react-dom";
 import App from "./components/App";
-import "./index.css";
+import GlobalStyles from "./components/GlobalStyles";
 
 render(
   <StrictMode>
     <App />
+    <GlobalStyles />
   </StrictMode>,
   document.getElementById("root")
 );
