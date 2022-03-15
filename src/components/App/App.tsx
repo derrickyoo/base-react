8bc6958555f471c549f0d0dffe6d67a4647e85ff
Derrick Yoo <derrickyoo@gmail.com>
2022-03-15 14:06:29 -0700

    Make base react app fun again

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,31 @@
+import styled from "styled-components/macro";
 import MaxWidthWrapper from "../MaxWidthWrapper";
 
 const App = () => {
   return (
     <>
       <MaxWidthWrapper>
-        <h1> Base React App</h1>
+        <AppHeader>Base React App</AppHeader>
+        <Subheading>Gradients, so hot right now.</Subheading>
       </MaxWidthWrapper>
     </>
   );
 };
+
+const AppHeader = styled.h1`
+  font-size: 4rem;
+  background-image: linear-gradient(
+    90deg,
+    hsl(47deg 95% 57%),
+    hsl(0deg 100% 65%)
+  );
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`;
+
+const Subheading = styled.p`
+  font-size: 1.25rem;
+`;
 
 export default App;
